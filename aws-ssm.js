382982090b1a8170ce706489d0bb16c376a8f7ff
skylor727 <skylor1997@hotmail.com>
2023-01-03 16:55:19 -0500

    update ssm code

--- aws-ssm.js
+++ aws-ssm.js
@@ -2,16 +2,19 @@ const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-2" });
 
 const ssm = new AWS.SSM();
+let SECRETS;
 
 const params = {
-  Name: "GOOGLE_CLIENT_ID",
+  Names: ["GOOGLE_CLIENT_ID", "GOOGLE_SECRET"],
   WithDecryption: true,
 };
 
-ssm.getParameter(params, (err, data) => {
+ssm.getParameters(params, (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data.Parameter.Value);
+    SECRETS = data.Parameters.Value;
   }
 });
+
+console.log(SECRETS);
