2a40ee17b3c875464b64cb4fd137281d88945c7c
skylor727 <skylor1997@hotmail.com>
2023-01-03 19:18:05 -0500

    update passport for ec2

--- aws-ssm.js
+++ aws-ssm.js
@@ -1,20 +1,28 @@
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-2" });
-
+require("dotenv").config();
+const key = process.env.ACCESS_KEY;
+const secret_key = process.env.SECRET_KEY;
+AWS.config.credentials = new AWS.Credentials(key, secret_key);
 const ssm = new AWS.SSM();
-let SECRETS;
 
-const params = {
-  Names: ["GOOGLE_CLIENT_ID", "GOOGLE_SECRET"],
-  WithDecryption: true,
+const getParameters = async () => {
+  const params = {
+    Names: ["GOOGLE_CLIENT_ID", "GOOGLE_SECRET", "GOOGLE_CALLBACK"],
+    WithDecryption: true,
+  };
+
+  try {
+    const data = await ssm.getParameters(params).promise();
+    return data.Parameters;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-ssm.getParameters(params, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    SECRETS = data.Parameters;
-  }
-});
+module.exports = getParameters;
 
-console.log(SECRETS);
+//const SECRETS = await getParameters();
+//const GOOGLE_CLIENT_ID = SECRETS[0].Value
+//const GOOGLE_SECRET = SECRETS[1].Value
+//const GOOGLE_CALLBACK = SECRETS[2].Value
--- config/passport.js
+++ config/passport.js
@@ -1,52 +1,61 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
 const pool = require("../db");
+const getSecrets = require("../aws-ssm");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK,
-    },
-    async (_, __, profile, done) => {
-      const account = profile._json;
-      let user = {};
-      try {
-        const currentUserQuery = await pool.query(
-          "SELECT * FROM users WHERE google_id=$1",
-          [account.sub]
-        );
-        //USER DOES NOT EXIST
-        if (currentUserQuery.rows.length === 0) {
-          await pool.query(
-            "INSERT INTO users (username, img, google_id) VALUES ($1,$2,$3)",
-            [account.name, account.picture, account.sub]
-          );
-          const id = await pool.query(
-            "SELECT id FROM users WHERE google_id=$1",
+const activatePassport = async () => {
+  const SECRETS = await getSecrets();
+  const GOOGLE_CLIENT_ID = SECRETS[0].Value;
+  const GOOGLE_SECRET = SECRETS[1].Value;
+  const GOOGLE_CALLBACK = SECRETS[2].Value;
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_SECRET,
+        callbackURL: GOOGLE_CALLBACK,
+      },
+      async (_, __, profile, done) => {
+        const account = profile._json;
+        let user = {};
+        try {
+          const currentUserQuery = await pool.query(
+            "SELECT * FROM users WHERE google_id=$1",
             [account.sub]
           );
-          user = {
-            id: id.rows[0].id,
-            username: account.name,
-            img: account.picture,
-          };
-        } else {
-          //USER ALREADY EXISTS
-          user = {
-            id: currentUserQuery.rows[0].id,
-            username: currentUserQuery.rows[0].username,
-            img: currentUserQuery.rows[0].img,
-          };
+          //USER DOES NOT EXIST
+          if (currentUserQuery.rows.length === 0) {
+            await pool.query(
+              "INSERT INTO users (username, img, google_id) VALUES ($1,$2,$3)",
+              [account.name, account.picture, account.sub]
+            );
+            const id = await pool.query(
+              "SELECT id FROM users WHERE google_id=$1",
+              [account.sub]
+            );
+            user = {
+              id: id.rows[0].id,
+              username: account.name,
+              img: account.picture,
+            };
+          } else {
+            //USER ALREADY EXISTS
+            user = {
+              id: currentUserQuery.rows[0].id,
+              username: currentUserQuery.rows[0].username,
+              img: currentUserQuery.rows[0].img,
+            };
+          }
+          done(null, user);
+        } catch (err) {
+          done(err);
         }
-        done(null, user);
-      } catch (err) {
-        done(err);
       }
-    }
-  )
-);
+    )
+  );
+};
+
+activatePassport();
 
 passport.serializeUser((user, done) => {
   done(null, user);
