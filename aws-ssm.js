aa68d643f867cc67dab25f9e8aa0f924076c0b7f
skylor727 <skylor1997@hotmail.com>
2023-01-04 10:40:41 -0500

    remove render files

---
+++ aws-ssm.js
@@ -0,0 +1,24 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-2" });
+require("dotenv").config();
+const key = process.env.ACCESS_KEY;
+const secret_key = process.env.SECRET_KEY;
+AWS.config.credentials = new AWS.Credentials(key, secret_key);
+const ssm = new AWS.SSM();
+
+const getParameters = async () => {
+  const names = ["GOOGLE_CLIENT_ID", "GOOGLE_SECRET", "GOOGLE_CALLBACK"];
+  const params = {
+    Names: names,
+    WithDecryption: true,
+  };
+  const data = await ssm.getParameters(params).promise();
+  return data.Parameters;
+};
+getParameters();
+module.exports = getParameters;
+
+//const SECRETS = await getParameters();
+//const GOOGLE_CLIENT_ID = SECRETS[0].Value
+//const GOOGLE_SECRET = SECRETS[1].Value
+//const GOOGLE_CALLBACK = SECRETS[2].Value
