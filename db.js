649b8e12ae324d79b8303965174c575a92b63797
skylor727 <skylor1997@hotmail.com>
2023-01-04 14:00:48 -0500

    try to fix oauth with ec2 instance

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 const { Pool } = require("pg");
 const getDBUrl = require("./aws-ssm").getParameter;
+const DATABASE_URL = await getDBUrl("DATABASE_URL");
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-async function main() {
-  const DATABASE_URL = await getDBUrl("DATABASE_URL");
-  const pool = new Pool({
-    connectionString: DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  module.exports = pool;
-}
-main();
+module.exports = pool;
